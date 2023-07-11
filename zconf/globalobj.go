8276ebbb3df829d6435bc44d1beee3925c0a21b5
小镇青年 <louyuexing@outlook.com>
2023-07-11 16:12:31 +0800

    init

--- zconf/globalobj.go
+++ zconf/globalobj.go
@@ -199,7 +199,6 @@ func init() {
 
 	// Initialize the GlobalObject variable and set some default values.
 	// (初始化GlobalObject变量，设置一些默认值)
-
 	GlobalObject = &Config{
 		Name:              "ZinxServerApp",
 		Version:           "V1.0",
